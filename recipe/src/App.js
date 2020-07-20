2eabb642f00f5406ddb64893535d7a3e116bf218
Alex Underwood <underwoodapu@gmail.com>
2020-07-19 22:16:13 -0700

    adjustments

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -12,15 +12,16 @@ const App = () => {
   const [query, setQuery] = useState('chicken');
     
   useEffect( () => {
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log(data.hits);
+    }
     getRecipes();
   }, [query]);
 
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  }
+
 
   const updateSearch = e => {
     setSearch(e.target.value);
@@ -34,6 +35,7 @@ const App = () => {
 
   return(
     <div className="App">
+      <h1 className="title">Search for a recipe!</h1>
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
@@ -43,14 +45,24 @@ const App = () => {
           <Recipe 
                   key={recipe.recipe.label}
                   title={recipe.recipe.label} 
-                  calories={recipe.recipe.calories} 
+                  yields={recipe.recipe.yield}
+                  calories={recipe.recipe.calories}
+                  protein={recipe.recipe.totalNutrients.PROCNT.quantity}
+                  carbs={recipe.recipe.totalNutrients.CHOCDF.quantity}
+                  fats={recipe.recipe.totalNutrients.FAT.quantity} 
                   image={recipe.recipe.image}
                   ingredients ={recipe.recipe.ingredients}
+                  warnings={recipe.recipe.healthLabels}
           />
         ))}
       </div>
-      
+      <div class="ad">
+        <div class="name">
+            <span id="watermark">created by @alexpunderwood</span>
+        </div>
+      </div>
     </div>
+    
   );
 };
 
--- recipe/src/recipe.js
+++ recipe/src/recipe.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import style from './recipe.module.css';
 
-const Recipe = ({title, calories, image, ingredients}) => {
+const Recipe = ({title, yields, calories, image, ingredients, warnings, protein, carbs, fats}) => {
     return(
         <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
+            <h1 className={style.title}>{title}</h1>
+            <p className={style.servings}>Servings: {yields}</p>
+            <ol className={style.ingredients}>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
-            <p>Calories: {calories}</p>
-            <img src={image} alt=""/>
-
+            <p className={style.calories} >Calories Per Serving: {Math.round(calories/yields)}</p>
+            <div >
+                <p className={style.macros}>Protein: {Math.round(protein/yields)}g</p>
+                <p className={style.macros}>Carbs: {Math.round(carbs/yields)}g</p>
+                <p className={style.macros}>Fats: {Math.round(fats/yields)}g</p>
+            </div>
+            <img className={style.image} src={image} alt=""/>
+            <div className={style.warning}>
+                <p>
+                    {warnings.map(warning =>(
+                        <span>| {warning} |</span>
+                    ))}
+                </p>
+            </div>
+            
         </div>
     );
 }
